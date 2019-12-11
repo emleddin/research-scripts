b702b26a0aabdf0d1888118ff8f22dba7ae96b50
emleddin <52934003+emleddin@users.noreply.github.com>
2019-12-11 09:57:52 -0600

    Create PBS script for Tinker-OpenMM
    Save repeated arcs in short chunks

---
+++ tinker-analysis/tinker-cuda.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#PBS -q my_gpu_alloc
+#PBS -l nodes=n01-02-03
+#PBS -j oe
+#PBS -r n
+#PBS -o err.error
+#PBS -N WT_protein_system
+
+xyz=WT_protein_system_prod.xyz
+key=WT_protein_system_prod.key
+
+export CUDA_VISIBLE_DEVICES=0
+
+cd $PBS_O_WORKDIR
+
+module load tinker/8.7.1/gcc550-cuda8
+
+arc=`echo "$xyz" | cut -d'.' -f1`
+
+## natom + 2 = linenumbers
+## read trims whitespace/carriage returns of first line
+read -r var < ${xyz}
+linenumber=$((var+2))
+
+## md = MD steps
+## ts = timestep in fs
+## int = time interval for printing in ps
+## sim = simulation ensemble (2=NVT)
+## temp = desired temperature in K
+## GPU = print GPU info
+md=250000
+ts=2.0
+int=0.5
+sim=2
+temp=300
+GPU=N
+
+e=1
+f=2
+
+## Copy the first one for looping
+cp $xyz ${xyz}_${e}
+
+## 500 ps = 0.5 ns = 1 file; 20 files is 10 ns
+while [ $e -lt 21 ]; do
+
+dynamic_omm ${xyz}_${e} -k $key $md $ts $int $sim $temp $GPU > prod$e.out
+
+## Resave the arc file (otherwise overwritten)
+mv ${arc}.arc ${arc}.arc_${e}
+
+## Save the next input XYZ
+tail -n ${linenumber} ${arc}.arc_${e} > ${xyz}_${f}
+
+e=$[$e+1]
+f=$[$f+1]
+
+done
